eb59086897dabf444cbed10a0dd72d308722fc04
HKeiju <keiju.love@gmail.com>
2020-04-21 12:05:06 +0900

    Added view and template for detailed blog post as well as CSS for the site.

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from . import views
 # post_list という名前の ビュー をルートURLに割り当てる
 urlpatterns = [
     path('',views.post_list,name='post_list'),
+    path('post/<int:pk>/',views.post_detail,name='post_detail'),
 ]
